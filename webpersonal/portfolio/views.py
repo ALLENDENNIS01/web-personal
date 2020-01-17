2baaf08714f0381d0602c04eb6e2c94f211cd147
ALLENDENNIS01 <allen.lapa@pucp.edu.pe>
2020-01-17 14:21:03 -0500

    Añadido proyecto

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -3,5 +3,5 @@ from .models import Project
 
 # Create your views here.
 def portfolio(request):
-    projects = P roject.objects.all()
+    projects = Project.objects.all()
     return render(request, "portfolio/portfolio.html", {'projects':projects})
